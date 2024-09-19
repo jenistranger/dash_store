1d13f668d70360c5d08ef6aea86db3979a8680d7
Evgeniy Mastenitsa <89588033+jenistranger@users.noreply.github.com>
2024-09-19 17:27:31 +0300

    Update db_main.py

--- db_main.py
+++ db_main.py
@@ -1,5 +1,6 @@
 import psycopg2
 from config_parser import load_config
+
 def connect():
     config = load_config()
     try:
@@ -9,6 +10,10 @@ def connect():
     except (psycopg2.DatabaseError, Exception) as error:
         print(error)
         return None
+
+
+
+
 def insert_data(query, data):
     conn = connect()
     if conn:
@@ -23,51 +28,70 @@ def insert_data(query, data):
         finally:
             conn.close()
             print("Connection closed.")
+
 # query = "INSERT INTO your_table (column1, column2) VALUES (%s, %s)"
 # data = ('value1', 'value2')
 # insert_data(query, data)
 import uuid
-if __name__ == '__main__':
-#Заполняем данные по периоду
-    query = """
-        INSERT INTO "periods" (
-        "period_type_guid",
-        "period_name"
+
+
+def upd_reference_table(table_name, column1, column2, data):
+    query = f"""
+        INSERT INTO "{table_name}" (
+        "{column1}",
+        "{column2}"
         )
         VALUES (%s, %s);    
-    """
-    periods_ = ("day", "month", "quarter", "year")
-    for period in periods_:
+    """ 
+    for item in data:
         random_uuid = str(uuid.uuid4())
-        data = (random_uuid, period)
-        insert_data(query, data)
-    # query = """
-    #     INSERT INTO "commercial_data" (
-    #     "_guid", 
-    #     "c_date", 
-    #     "period_type_guid", 
-    #     "asset_guid", 
-    #     "well_guid", 
-    #     "_location", 
-    #     "fluid_guid", 
-    #     "_value", 
-    #     "unit_guid", 
-    #     "license", 
-    #     "info_type_guid"
-    #     ) 
-    #     VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s);
-    # """
-    # data = (
-    #     'test-guid-001',    # _guid
-    #     '2024-09-18',       # c_date
-    #     'period-guid-001',  # period_type_guid
-    #     'asset-guid-001',   # asset_guid
-    #     'well-guid-001',    # well_guid
-    #     'Test Location',    # _location
-    #     'fluid-guid-001',   # fluid_guid
-    #     100.5,              # _value
-    #     'unit-guid-001',    # unit_guid
-    #     'Test License',     # license
-    #     'info-guid-001'     # info_type_guid
-    # )
-    # insert_data(query, data)
+        temp_data = (random_uuid, item)
+        insert_data(query, temp_data)
+
+
+def load_countries():
+    conn = connect()
+    if conn:
+        try:
+            with conn.cursor() as cur:
+                cur.execute("SELECT country_name FROM countries")
+                record = cur.fetchall()
+                
+                return record
+                
+        except (psycopg2.DatabaseError, Exception) as error:
+            print(f"Error occurred during insert: {error}")
+            conn.rollback()
+        finally:
+            conn.close()
+            print("Connection closed.")
+
+def get_country_guid(target):
+    conn = connect()
+    if conn:
+        try:
+            with conn.cursor() as cur:
+                cur.execute(f"SELECT country_guid FROM countries WHERE country_name = \'{target}\'")
+                record = cur.fetchone()[0]
+                return record
+        except (psycopg2.DatabaseError, Exception) as error:
+            print(f"Error occurred during insert: {error}")
+            conn.rollback()
+        finally:
+            conn.close()
+            print("Connection closed.")
+
+
+if __name__ == '__main__':
+
+
+    pass
+    # assets_data = [
+    #         (str(uuid.uuid4()), 'Актив 1', str(get_country_guid("Боливия")), 10),
+    #         (str(uuid.uuid4()), 'Актив 2', str(get_country_guid("Вьетнам")), 15),
+    #         (str(uuid.uuid4()), 'Актив 3', str(get_country_guid("Алжир")), 8)
+    #     ]
+    
+    # for a_data in assets_data:
+    #     query = ("INSERT INTO assets (asset_guid, asset_name, country_guid, total_wells) VALUES (%s, %s, %s, %s)")
+    #     insert_data(query, a_data)
