e56bfa895011b1bf1bb938d5e61b55c710483c42
Evgeniy Mastenitsa <89588033+jenistranger@users.noreply.github.com>
2024-10-04 17:16:09 +0300

    Create devapp.py

---
+++ devapp.py
@@ -0,0 +1,72 @@
+
+import dash
+from dash import Dash, dcc, html, Input, Output, callback
+import dash_bootstrap_components as dbc
+import secrets
+from flask import Flask
+
+server = Flask(__name__)
+server.secret_key = secrets.token_hex(16)
+server.config["SESSION_PERMANENT"] = False
+server.config["SESSION_TYPE"] = "filesystem"
+
+app = Dash(server=server,
+           url_base_pathname='/',
+           suppress_callback_exceptions=True,
+           assets_folder='assets',
+           title='Пример приложения',
+           use_pages=True,
+           pages_folder='pages'
+           )
+
+
+navbar = dbc.NavbarSimple(
+    children=[
+        dbc.DropdownMenu(
+            children=[
+                        dbc.DropdownMenuItem(page['name'], href=page['path']) 
+                        for page in dash.page_registry.values()
+                     ],
+            nav=True,
+            in_navbar=True,
+            label="Меню"
+        )
+    ],
+    # children=[
+    #     dbc.NavItem(dbc.NavLink("Page 1", href="#")),
+    #     dbc.DropdownMenu(
+    #         children=[
+    #             dbc.DropdownMenuItem("More pages", header=True),
+    #             dbc.DropdownMenuItem("Page 2", href="#"),
+    #             dbc.DropdownMenuItem("Page 3", href="#"),
+    #         ],
+    #         nav=True,
+    #         in_navbar=True,
+    #         label="More",
+    #     ),
+    # ],
+    brand="Приложение",
+    brand_href="/",
+)
+upper_sidebar = html.Div([html.H4("Навигация"), navbar],
+                         className="upper_sidebar")
+
+bottom_sidebar = html.Div(children=[html.H4("Версия 0.1")
+], className='bottom_sidebar')
+
+main_content = html.Div(dash.page_container,className="main_content")
+
+app.layout = html.Div([
+    upper_sidebar,
+    navbar,
+    main_content,
+    bottom_sidebar
+])
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=53996, debug=True)
+
+
+
+
+
